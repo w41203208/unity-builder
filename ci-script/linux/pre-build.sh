300f1ebc70e543db0ff416b2343db478a02ec8ae
Jay <w41203208@gmail.com>
2023-12-28 14:59:20 +0800

    update pre-build.sh

--- ci-script/linux/pre-build.sh
+++ ci-script/linux/pre-build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# will change this code
+path="$(pwd)/Coin-Pusher-Dannis"
+if [ -e "$path"]
+  rmdir -r "$path"
+fi
+
 git clone $1 --branch webRTC-dev-feature-buildtool --depth=1
 
 mkdir "build/Coin Pusher"
