5c625e71cf347ffcb71c0d0742fde4a6fc9196c8
Jay <w41203208@gmail.com>
2024-01-03 14:30:43 +0800

    test use archiveArtifacts

--- ci-script/linux/pre-build.sh
+++ ci-script/linux/pre-build.sh
@@ -13,7 +13,7 @@ fi
 # git clone $1 --branch webRTC-dev-feature-buildtool --depth=1
 
 path="build/$UNITY_PROJECT_BUILD_FOLDER_NAME"
-if [ ! -d "$path" ]; then
+if [ ! -e "$path" ]; then
   mkdir "$path"
 fi
 
