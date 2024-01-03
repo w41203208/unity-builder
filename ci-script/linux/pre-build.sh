418623e8fb8bf6f23d33eff98e65a13422a59301
Jay <w41203208@gmail.com>
2024-01-03 16:16:56 +0800

    fix pre-build step that path is not indicated current path

--- ci-script/linux/pre-build.sh
+++ ci-script/linux/pre-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # will change this code
+original_path="$(pwd)"
 path="$(pwd)/$UNITY_PROJECT_NAME"
 if [ -e "$path" ]; then
   cd "$path"
@@ -10,10 +11,12 @@ else
   git clone $1 --branch webRTC-dev-feature-buildtool --depth=1
 fi
 
+cd "$original_path"
+
 # git clone $1 --branch webRTC-dev-feature-buildtool --depth=1
 
 path="build/$UNITY_PROJECT_BUILD_FOLDER_NAME"
-if [ ! -e "$path" ]; then
+if [ ! -d "$path" ]; then
   mkdir "$path"
 fi
 
